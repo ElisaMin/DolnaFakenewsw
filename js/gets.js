81424bee41f9ac1101c7575476ff7715d256e9f6
Heizi <35397814+ElisaMin@users.noreply.github.com>
2020-07-25 18:56:27 +0800

    依旧备份

--- js/gets.js
+++ js/gets.js
@@ -2,4 +2,19 @@
 cookie = new Map();function updateCookie(){let tmp = document.cookie;tmp.split("; ").forEach( it => {let a = it.split("=");cookie.set(a[0],a[1]);})}function getCookie(name){if (!cookie) updateCookie();return cookie.get(name);}updateCookie();function setCookie(cookieName,value){cookie.set(cookieName,value);document.cookie = cookieName+"="+value+";";}
 
 //getLang
+
 var currentLang = lang.zh;
+function setLang(l){
+	switch (l) {
+		case "zh":currentLang = lang.zh;break;
+		case "en":currentLang = lang.en;break;
+		case "hz":currentLang = lang.hz;break;
+	}
+	setCookie("lang",l);
+}
+let l = getCookie("lang");
+if (!l) {
+	setLang("zh");
+}else{
+	setLang(getCookie("lang"))
+}
---
+++ js/lang.js
@@ -0,0 +1,22 @@
+var lang = {
+	zh:{
+		tag:{
+			head:"首页",
+			more:"更多",
+			sett:"设置",
+			abme:"关于"
+			
+		},
+		fmt_404:"<h2>页面不存在</h2>"
+	},
+	en:{
+		tag:{
+			head:"HEAD",
+			more:"MORE",
+			sett:"CUST",
+			abme:"ABOUT"
+			
+		},
+		fmt_404:"<h2>页面不存在</h2>"
+	}
+}
