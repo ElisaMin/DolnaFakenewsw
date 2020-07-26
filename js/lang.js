6e7f86cc839aca7dd1d9cb154bdf363e91bccacb
Heizi <35397814+ElisaMin@users.noreply.github.com>
2020-07-26 17:05:05 +0800

    完善设置语言
    新增显示结果的dialog

--- js/lang.js
+++ js/lang.js
@@ -1,5 +1,8 @@
 var lang = {
 	zh:{
+		def:{
+			close:"关闭"
+		},
 		tag:{
 			head:"首页",
 			more:"更多",
@@ -7,9 +10,16 @@ var lang = {
 			abme:"关于"
 			
 		},
+		result:{
+			lang: "修改成功，请刷新页面查看结果。",
+			unknowerr:"未知错误"
+		},
 		fmt_404:"<h2>页面不存在</h2>"
 	},
 	en:{
+		def:{
+			close:"close"
+		},
 		tag:{
 			head:"HEAD",
 			more:"MORE",
@@ -17,6 +27,10 @@ var lang = {
 			abme:"ABOUT"
 			
 		},
-		fmt_404:"<h2>页面不存在</h2>"
+		result:{
+			lang: "the language is changed , plz relaod this web.",
+			unknowerr:"unknow errow !"
+		},
+		fmt_404:"<h2>Page Not Found</h2>"
 	}
 }
